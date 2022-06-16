fe63612eaa1cf7a10653b02715966349495695f5
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-06-16 14:28:48 +0430

    finish

--- file_path.go
+++ file_path.go
@@ -8,18 +8,24 @@ import (
 
 func main() {
 
+	// Join should be used to construct paths in a portable way.
+	// It takes any number of arguments and constructs a hierarchical path from them.
+
 	p := filepath.Join("dir1", "dir2", "filename")
 	fmt.Println("p:", p)
 
+	// You should always use Join instead of concatenating /s or \s manually.
+	// In addition to providing portability, Join will also normalize paths by removing superfluous separators and directory changes.
 	fmt.Println(filepath.Join("dir1//", "filename"))
 	fmt.Println(filepath.Join("dir1/../dir1", "filename"))
 
 	fmt.Println("Dir(p):", filepath.Dir(p))
 	fmt.Println("Base(p):", filepath.Base(p))
-
+	// We can check whether a path is absolute.
 	fmt.Println(filepath.IsAbs("dir/file"))
 	fmt.Println(filepath.IsAbs("/dir/file"))
 
+	// Dir and Base can be used to split a path to the directory and the file. Alternatively, Split will return both in the same call.
 	filename := "config.json"
 
 	ext := filepath.Ext(filename)
@@ -40,6 +46,9 @@ func main() {
 	fmt.Println(rel)
 }
 
+// Some file names have extensions following a dot. We can split the extension out of such names with Ext.
+
+// Rel finds a relative path between a base and a target. It returns an error if the target cannot be made relative to base.
 // output :
 
 // p: dir1/dir2/filename
