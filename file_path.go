61e00ef58d089478fae66767f7bbe9f83a30e6e2
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-06-16 14:25:09 +0430

    output

--- file_path.go
+++ file_path.go
@@ -39,3 +39,17 @@ func main() {
 	}
 	fmt.Println(rel)
 }
+
+// output :
+
+// p: dir1/dir2/filename
+// dir1/filename
+// dir1/filename
+// Dir(p): dir1/dir2
+// Base(p): filename
+// false
+// true
+// .json
+// config
+// t/file
+// ../c/t/file
